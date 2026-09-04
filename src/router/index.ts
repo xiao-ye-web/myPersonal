0fc8c2ae736b0f2350b371d995ae37e4216da18c
xiaoye <3472493063@qq.com>
2026-09-04 08:05:02 +0800

    初始化git，配置项目依赖

---
+++ src/router/index.ts
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from 'vue-router'
+// 实例化路由器
+const router = createRouter({
+  //配置路由工作状态为history模式
+  history: createWebHistory(),
+  //一堆路由配置
+  routes: [],
+})
+
+export default router
